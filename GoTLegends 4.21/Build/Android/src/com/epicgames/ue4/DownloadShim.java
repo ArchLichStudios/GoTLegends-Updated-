63443949d0de91ef22ee6f3bac3fa13669a42b8f
ArchLichStudios <46579485+ArchLichStudios@users.noreply.github.com>
2019-05-03 04:09:22 -0400

    Commit 0.0.2.7
    Added main menu UI

--- GoTLegends 4.21/Build/Android/src/com/epicgames/ue4/DownloadShim.java
+++ GoTLegends 4.21/Build/Android/src/com/epicgames/ue4/DownloadShim.java
@@ -1,10 +1,10 @@
 package com.epicgames.ue4;
 
-import com.ArchlichStudios.GoTLegends.OBBDownloaderService;
-import com.ArchlichStudios.GoTLegends.DownloaderActivity;
+import com.YourCompany.GoTLegends.OBBDownloaderService;
+import com.YourCompany.GoTLegends.DownloaderActivity;
 import android.app.Activity;
 import com.google.android.vending.expansion.downloader.Helpers;
-import com.ArchlichStudios.GoTLegends.OBBData;
+import com.YourCompany.GoTLegends.OBBData;
 
 
 public class DownloadShim
