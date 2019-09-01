cb8694a47b3faabb5d9a454346dd489e2310a857
ArchLichStudios <46579485+ArchLichStudios@users.noreply.github.com>
2019-09-01 03:08:57 -0400

    Commit 0.1.7.8

--- GoTLegends 4.21/Build/Android/src/com/ArchLichStudios/GOTLegends/OBBData.java
+++ GoTLegends 4.21/Build/Android/src/com/ArchLichStudios/GOTLegends/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"1", // the version of the APK that the file was uploaded against
-1822209068L // the length of the file in bytes
+"3", // the version of the APK that the file was uploaded against
+2114969779L // the length of the file in bytes
 )
 };
 };
